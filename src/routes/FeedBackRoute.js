b331c85ce920816a54d1e6241c77b496a6e713ed
abhijan2402 <abhishek.jangid643@gmail.com>
2023-06-25 23:27:33 +0530

    adding osme chnage

--- src/routes/FeedBackRoute.js
+++ src/routes/FeedBackRoute.js
@@ -5,8 +5,8 @@ const { v4: uuidv4 } = require('uuid');
 
 Feedback.post("/feedback", (req, res) => {
     console.log(req.body)
-    client.query("INSERT INTO Feedback (id, Fmessage,frating,shopid,senderid) VALUES ($1, $2 ,$3, $4,$5 ) RETURNING *",
-        [uuidv4(), req.body.Fmessage, req.body.frating, req.body.shopid, req.body.senderid,], (err, data) => {
+    client.query("INSERT INTO Feedback (id, Fmessage,frating,shopid,orderid) VALUES ($1, $2 ,$3, $4,$5 ) RETURNING *",
+        [uuidv4(), req.body.Fmessage, req.body.frating, req.body.shopid, req.body.orderid,], (err, data) => {
             if (err) {
                 console.log("hi");
                 res.send({ data: err, message: "Problem" })
