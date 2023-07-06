8d137513b8a2e15e4972d1010b37f2b5e37c8173
abhijan2402 <abhishek.jangid643@gmail.com>
2023-07-06 10:04:48 +0530

    Adding some changes

--- src/routes/global/Update.js
+++ src/routes/global/Update.js
@@ -1,32 +1,32 @@
 const express = require('express');
 const client = require('../../database');
 const updateDataInTable = express.Router();
-    
-updateDataInTable.post("/update", (req, res) => {
-    const {id,updateData,tablename}=req.body;
 
-    let updatedFields=Object.entries(updateData);
-    let queryStringUpdateFields='';
-    if(updatedFields.length===1){
-        queryStringUpdateFields=`${updatedFields[0][0]}='${updatedFields[0][1]}'`
+updateDataInTable.post("/update", (req, res) => {
+    const { id, updateData, tablename } = req.body;
+
+    let updatedFields = Object.entries(updateData);
+    let queryStringUpdateFields = '';
+    if (updatedFields.length === 1) {
+        queryStringUpdateFields = `${updatedFields[0][0]}='${updatedFields[0][1]}'`
     }
-    else{
+    else {
         for (let i = 0; i < updatedFields.length; i++) {
-            if(i===updatedFields.length-1){
-                queryStringUpdateFields=queryStringUpdateFields.concat(`${updatedFields[i][0]}='${updatedFields[i][1]}'`)
+            if (i === updatedFields.length - 1) {
+                queryStringUpdateFields = queryStringUpdateFields.concat(`${updatedFields[i][0]}='${updatedFields[i][1]}'`)
             }
-            else{
-                queryStringUpdateFields=queryStringUpdateFields.concat(`${updatedFields[i][0]}='${updatedFields[i][1]}',`)
+            else {
+                queryStringUpdateFields = queryStringUpdateFields.concat(`${updatedFields[i][0]}='${updatedFields[i][1]}',`)
             }
         }
     }
-    let finalQuery=`UPDATE ${tablename} SET ${queryStringUpdateFields} WHERE id = '${id}'`
+    let finalQuery = `UPDATE ${tablename} SET ${queryStringUpdateFields} WHERE id = '${id}'`
     client.query(finalQuery, (err, data) => {
         if (err) {
             res.send({ data: err })
         }
         else {
-            res.send({ data: data.rows })
+            res.send({ data: data.rows, message: "Your data is updated" })
         }
     })
 });
