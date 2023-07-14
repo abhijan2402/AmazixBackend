3e6bddae562e8ab426422e4709e4d05d951e41a0
abhijan2402 <abhishek.jangid643@gmail.com>
2023-07-14 13:47:20 +0530

    Adding vercel

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 const express = require("express");
 const app = express();
 const { createServer } = require("http");
-const client = require("./src/database");
-const Coupen = require("./src/routes/coupenRoute");
-const getAllChats = require("./src/routes/chat/getChats");
-const getOrders = require("./src/routes/orders/getOrders");
-const deleteChats = require("./src/routes/chat/delete");
-const deleteOrder = require("./src/routes/orders/delete");
-const addNewOrder = require("./src/routes/orders/create");
-const addNewChat = require("./src/routes/chat/create");
-const seller = require("./src/routes/sellerroute");
-const Feedback = require("./src/routes/FeedBackRoute");
-const Wallet = require("./src/routes/walletRoute");
-const Notification = require("./src/routes/NotificationRoute");
+// const client = require("./src/database");
+// const Coupen = require("./src/routes/coupenRoute");
+// const getAllChats = require("./src/routes/chat/getChats");
+// const getOrders = require("./src/routes/orders/getOrders");
+// const deleteChats = require("./src/routes/chat/delete");
+// const deleteOrder = require("./src/routes/orders/delete");
+// const addNewOrder = require("./src/routes/orders/create");
+// const addNewChat = require("./src/routes/chat/create");
+// const seller = require("./src/routes/sellerroute");
+// const Feedback = require("./src/routes/FeedBackRoute");
+// const Wallet = require("./src/routes/walletRoute");
+// const Notification = require("./src/routes/NotificationRoute");
 const port = process.env.PORT || 3000
 const envVariables = require('dotenv');
-const category = require("./src/routes/Category/Category");
-const product = require("./src/routes/Product/Product");
-const updateDataInTable = require("./src/routes/global/Update");
-const getTableData = require("./src/routes/global/get");
-const DeleteTableData = require("./src/routes/global/Delete");
-const store = require("./src/routes/seller/store");
-const Banner = require("./src/routes/BannerRoutes");
-const Loginhistory = require("./src/routes/LoginHistory/LoginHistory");
-const Address = require("./src/routes/AddressRoute");
-const Customer = require("./src/routes/Customer/Customer");
-const Handlecash = require("./src/routes/HandlecashRoute");
-const Cart = require("./src/routes/Cart/Cart");
-const DeliveryRegis = require("./src/routes/DeliveryRegisteration");
-const StartDuty = require("./src/routes/StartDutyRoute");
+// const category = require("./src/routes/Category/Category");
+// const product = require("./src/routes/Product/Product");
+// const updateDataInTable = require("./src/routes/global/Update");
+// const getTableData = require("./src/routes/global/get");
+// const DeleteTableData = require("./src/routes/global/Delete");
+// const store = require("./src/routes/seller/store");
+// const Banner = require("./src/routes/BannerRoutes");
+// const Loginhistory = require("./src/routes/LoginHistory/LoginHistory");
+// const Address = require("./src/routes/AddressRoute");
+// const Customer = require("./src/routes/Customer/Customer");
+// const Handlecash = require("./src/routes/HandlecashRoute");
+// const Cart = require("./src/routes/Cart/Cart");
+// const DeliveryRegis = require("./src/routes/DeliveryRegisteration");
+// const StartDuty = require("./src/routes/StartDutyRoute");
 
 envVariables.config()
 
@@ -42,7 +42,7 @@ app.use(express.urlencoded({
 
 httpServer.listen(port, () => {
   console.log(`Server started at ${port}`)
-  client.connect()
+  // client.connect()
 });
 
 
@@ -51,71 +51,71 @@ app.use("/", (req, res) => {
   res.send({ message: "Hii" })
 });
 
-app.use(seller)
-//Coupen
-app.use(Coupen);
+// app.use(seller)
+// //Coupen
+// app.use(Coupen);
 
 
-//Feedback
-app.use(Feedback)
+// //Feedback
+// app.use(Feedback)
 
 
-//wallet
-app.use(Wallet)
+// //wallet
+// app.use(Wallet)
 
-//notif
-app.use(Notification)
+// //notif
+// app.use(Notification)
 
-//chats
-app.use(getAllChats);
-app.use(deleteChats);
-app.use(addNewChat);
+// //chats
+// app.use(getAllChats);
+// app.use(deleteChats);
+// app.use(addNewChat);
 
-//order
-app.use(getOrders);
-app.use(deleteOrder);
-app.use(addNewOrder);
+// //order
+// app.use(getOrders);
+// app.use(deleteOrder);
+// app.use(addNewOrder);
 
-//update
-app.use(updateDataInTable)
+// //update
+// app.use(updateDataInTable)
 
-//cateory
-app.use(category)
+// //cateory
+// app.use(category)
 
-//store
-app.use(store);
+// //store
+// app.use(store);
 
-//product
-app.use(product)
+// //product
+// app.use(product)
 
-//Banner
-app.use(Banner)
+// //Banner
+// app.use(Banner)
 
-//Address
-app.use(Address)
+// //Address
+// app.use(Address)
 
-//get
-app.use(getTableData)
+// //get
+// app.use(getTableData)
 
-//delete
-app.use(DeleteTableData)
+// //delete
+// app.use(DeleteTableData)
 
-//loginhistory
-app.use(Loginhistory)
+// //loginhistory
+// app.use(Loginhistory)
 
-//customer
-app.use(Customer)
+// //customer
+// app.use(Customer)
 
-app.use(Handlecash)
+// app.use(Handlecash)
 
-//cart
-app.use(Cart);
+// //cart
+// app.use(Cart);
 
-//Handlecash
-app.use(Handlecash)
+// //Handlecash
+// app.use(Handlecash)
 
-//DeliveryRegis
-app.use(DeliveryRegis)
+// //DeliveryRegis
+// app.use(DeliveryRegis)
 
-//startDuty
-app.use(StartDuty)
+// //startDuty
+// app.use(StartDuty)
