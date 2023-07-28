8a50804fb04c930c7dfde1e158c126acbb9eaa63
Chirag <takchirag828@gmail.com>
2023-07-28 21:42:02 +0530

    some

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 const { createServer } = require("http");
-const port = process.env.PORT
+const port = process.env.PORT || 3001
 const { connectDB,client } = require("./database");
 const {v4 : uuidv4} = require('uuid');
 envVariables.config()
