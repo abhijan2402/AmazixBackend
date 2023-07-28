fba1bc9d2d7086a7d6f0355df982074a7fac0684
Chirag <takchirag828@gmail.com>
2023-07-28 21:35:56 +0530

    some env changes

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const { Client } = require('pg');
 require('dotenv').config();
-console.log(process.env.USER)
+console.log(process.env.USERNAMEDB)
 console.log(process.env.PASSWORD)
 console.log(process.env.DBNAME)
 console.log(process.env.CONNECTIONSTRING)
@@ -13,14 +13,14 @@ const client = new Client({
 });
 
 async function connectDB(){
-    try {
-        client.connect(function(err) {
-            if (err) throw err;
-            console.log("Connected!");
-        });        
-    } catch (error) {
-        console.log(error,"Error");
-    }
+    // try {
+    //     client.connect(function(err) {
+    //         if (err) throw err;
+    //         console.log("Connected!");
+    //     });        
+    // } catch (error) {
+    //     console.log(error,"Error");
+    // }
 }
 
 module.exports = {connectDB,client};
